6871cd8f16364a3323d19986f10f96e833787d97
Mehmet Yazicioglu <mehmetytx@gmail.com>
2019-03-24 16:52:40 -0500

    Adding new codes

---
+++ dallas-cybertek/src/test/java/tests/MavenTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.github.javafaker.Faker;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class MavenTest {
+public static void main(String[] args) {
+	
+	
+		Faker faker = new Faker();
+		System.out.println(faker.address().city());
+		
+		System.out.println(faker.name().name());
+		System.out.println(faker.phoneNumber().cellPhone());
+		
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://google.com");
+		//driver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div/div[1]/div/div[1]/input")).sendKeys(faker.name().name()+Keys.ENTER);
+		driver.findElement(By.name("q")).sendKeys(faker.name().name()+Keys.ENTER);
+
+		
+}
+}
